e803e5daf8b152d578a04139e6a7332b3b31897e
Ryan Zeigler <ryan.zeigler@taillight.com>
2016-10-14 13:50:07 -0500

    Curry the bind functions.

--- index.ts
+++ index.ts
@@ -124,21 +124,25 @@ export function bindProperty(key: string, on: any) {
 }
 
 export function bindProjection(projection: string[], on: any, value: any) {
-    projection.forEach(key => {
-        on[key] = value[key];
-    });
+    return function (value: any) {
+        projection.forEach(key => {
+            on[key] = value[key];
+        });
+    }
 }
 
-export function bindFormValues(projection: string[], on: FormGroup, source: any) {
-    projection.forEach(key => {
-        if (!on.controls[key]) {
-            console.warn(`Disregarding form binding for ${key}. No control exists`);
-        }
-        // Prevent change spinning
-        if (on.controls[key].value !== source[key]) {
-            on.controls[key].setValue(source[key]);
-        }
-    });
+export function bindFormValues(projection: string[], on: FormGroup) {
+    return function(source: any) {
+        projection.forEach(key => {
+            if (!on.controls[key]) {
+                console.warn(`Disregarding form binding for ${key}. No control exists`);
+            }
+            // Prevent change spinning
+            if (on.controls[key].value !== source[key]) {
+                on.controls[key].setValue(source[key]);
+            }
+        });
+    }
 }
 
 export function bindStore(store: {dispatch: Function}) {
